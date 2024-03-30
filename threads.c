e2ebc017910e7574cdfb07c0db8bcde14aceefe4
Julian Waksmann <96151663+JulianWaksmann@users.noreply.github.com>
2024-03-30 09:53:58 -0300

    Update threads.c

--- threads.c
+++ threads.c
@@ -1,17 +1,19 @@
-#include <stdio.h> //incluimos la libreria de estandar input/output
-#include <unistd.h> //para hacer sleep
-#include <time.h> //para inicializar el tiempo
-void calculo_aritmetico(){
- int contador=0;
- while(contador < 2147483647){
- contador=contador+1;
- }
+#include <stdio.h> // Incluimos la librería de entrada/salida estándar
+#include <unistd.h> // Para hacer sleep (aunque no se usa en este fragmento)
+#include <time.h> // Para inicializar el tiempo (aunque no se usa en este fragmento)
+
+void calculo_aritmetico() {
+    int contador = 0;
+    while (contador < 2147483647) {
+        contador = contador + 1;
+    }
 }
-int main() {`1
- calculo_aritmetico();
- calculo_aritmetico();
- calculo_aritmetico();
- calculo_aritmetico();
- calculo_aritmetico();
- return 0;
+
+int main() {
+    calculo_aritmetico();
+    calculo_aritmetico();
+    calculo_aritmetico();
+    calculo_aritmetico();
+    calculo_aritmetico();
+    return 0;
 }
